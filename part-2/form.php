f8df4ae478e270464c545e3da7365aa90c520c97
weiqilu <luweiqi@hotmail.sg>
2019-08-02 12:00:15 +0800

    Added PHP form

---
+++ part-2/form.php
@@ -0,0 +1,44 @@
+<?php
+
+$success = 0;
+
+$link = mysqli_connect("localhost", "root", "", "test");
+
+if (isset($_POST['username'], $_POST['password'])) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$query = mysqli_query($link, "SELECT * FROM `login` WHERE `username` = '$username' AND `password` = '$password'");
+
+	if (mysqli_num_rows($query)) $success = 1;
+
+	if (!$success) print "Fail!";
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<?php if (!$success) { ?>
+	<h1>Login</h1>
+	<form method="post">
+		<div>
+			<label for="username">Username</label>
+			<input type="text" id="username" name="username">
+		</div>
+		<div>
+			<label for="password">Password</label>
+			<input type="password" id="password" name="password">
+		</div>
+		<button type="submit">LOGIN</button>
+	</form>
+	<?php } else { print "<h1>Welcome!</h1>"; } ?>
+</body>
+
+</html>
